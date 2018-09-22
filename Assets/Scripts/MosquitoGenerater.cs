54df7ba05315527f181071f612fb9489052ca8bb
shira <shira7867@gmail.com>
2018-09-23 00:09:18 +0900

    9/22作業分

---
+++ Assets/Scripts/MosquitoGenerater.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using System;
+
+public class MosquitoGenerater : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mosquito;
+
+    // Use this for initialization
+    void Start()
+    {
+        Observable.Interval(TimeSpan.FromMilliseconds(1000))
+            .Subscribe(_ =>
+            {
+                Instantiate(mosquito);
+            });
+    }
+}
