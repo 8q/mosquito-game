f738dccbd9fe835c5d5740c2a56f65f25e03e85f
shira <shira7867@gmail.com>
2018-09-22 21:05:38 +0900

    作業分

---
+++ Assets/Scripts/CursorBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class CursorBehaviour : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject pan;
+
+    // Use this for initialization
+    void Start()
+    {
+        // デフォルトのカーソルを消す
+        Cursor.visible = false;
+
+        // 疑似カーソルをマウスの位置に追従させる
+        this.UpdateAsObservable()
+            .Subscribe(_ =>
+            {
+                var targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                targetPosition.z = 0f;
+                transform.position = targetPosition;
+            })
+            .AddTo(gameObject);
+
+        // 左クリックしたときにする動作
+        var mouseDown = this.UpdateAsObservable()
+            .Where(_ => Input.GetMouseButtonDown(0));
+
+        // 「パン」を生成する。
+        mouseDown
+            .Subscribe(_ =>
+            {
+                Instantiate(pan, transform.position, pan.transform.rotation);
+            })
+            .AddTo(gameObject);
+
+        // クリックしたときにカーソルの下に蚊がいたら蚊を消す
+        mouseDown
+            .Select(_ => Physics2D.OverlapPoint(transform.position))
+            .Where(collider => collider != null && collider.gameObject.tag == "Mosquito")
+            .Subscribe(collider =>
+            {
+                Destroy(collider.gameObject);
+            })
+            .AddTo(gameObject);
+    }
+}
---
+++ Assets/Scripts/PanBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class PanBehaviour : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // １秒で殺す
+        Observable.Timer(TimeSpan.FromSeconds(1))
+            .Subscribe(_ =>
+            {
+                Destroy(gameObject);
+            })
+            .AddTo(gameObject);
+    }
+
+}
